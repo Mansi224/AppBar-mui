b21e465abf5692f32e4404ddfd0336ca61a2cf64
mansi <ladkarmansi@gmail.com>
2022-01-14 17:36:15 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import Appbar from "./Appbar";
+import menu from "./Menu";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Appbar />
+    </div>
+  );
 }
 
 export default App;
